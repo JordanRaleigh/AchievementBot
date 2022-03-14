7ca2e97539f799a9100e7acc89d2a94df8b8cf00
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2022-03-14 15:56:10 -0500

    Created API with GET routes

---
+++ pages/api/achievements.ts
@@ -0,0 +1,31 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+export default handler;
+
+const mockAchievement = {
+  user: "bob",
+  text: "Got into PIE",
+};
+const URI = `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.8e8eh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+async function handler(req, res) {
+  switch (req.method) {
+    case "GET":
+      const client = new MongoClient(URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverApi: ServerApiVersion.v1,
+      });
+
+      await client.connect();
+
+      const db = client.db("sample_mflix");
+
+      const query = { username: "michaelsholty" };
+
+      const achievement = await db.collection("achievements").findOne(query);
+
+      console.log(achievement);
+
+      res.status(200).json(achievement);
+  }
+}
