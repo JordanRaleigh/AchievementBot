78613c5081c7f6fd84592221cb3683bf03dfb8d1
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2022-03-15 16:47:38 -0500

    completed POST route, change GET to find all by username, added front end to list achievements

--- pages/api/achievements.ts
+++ pages/api/achievements.ts
@@ -22,17 +22,20 @@ async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
 
   switch (req.method) {
     case "GET":
-      const query = { username: "jordanraleigh" };
+      const query = { username: "jordan.raleigh" };
 
-      const achievement = await db.collection("achievements").findOne(query);
+      const achievements = await db
+        .collection("achievements")
+        .find(query)
+        .toArray();
 
-      console.log(achievement);
+      // console.log(achievement);
 
-      res.status(200).json(achievement);
+      return res.status(200).json(achievements);
 
     case "POST":
       const result = await db.collection("achievements").insertOne(req.body);
 
-      res.status(200).json(result);
+      return res.status(200).json(result);
   }
 }
