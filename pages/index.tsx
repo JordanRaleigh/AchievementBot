e8ca71b11a036015d79329870cd6a1d131ec1e4c
Jordan Raleigh <60301522+JordanRaleigh@users.noreply.github.com>
2022-04-02 16:21:04 -0500

    added an inspirational quote that pulls from api and displays on app

--- pages/index.tsx
+++ pages/index.tsx
@@ -53,6 +53,18 @@ const Home: NextPage = (props: any) => {
           })}
         </ul>
       </div>
+      <div>
+        <p>
+          {props.quote.map(function (quote: any) {
+            return (
+              <div key={"1"}>
+                <h3>{quote.q}</h3>
+                <h4>{quote.a}</h4>
+              </div>
+            );
+          })}
+        </p>
+      </div>
     </>
   );
 };
@@ -62,9 +74,15 @@ Home.getInitialProps = async (ctx) => {
 
   const res = await fetch(`${config.publicRuntimeConfig.apiUrl}/achievements`);
 
-  const json = await res.json();
+  const achievementsJson = await res.json();
 
-  return { achievements: json };
+  const quoteResponse = await fetch(
+    `https://zenquotes.io/api/today/${config.publicRuntimeConfig.quoteKey}`
+  );
+
+  const quoteJson = await quoteResponse.json();
+
+  return { achievements: achievementsJson, quote: quoteJson };
 };
 
 export default Home;
